fa6ad62d89b6e833ed7a262ae2606bca0f2f2352
MyuriAnily <eltinary@gmail.com>
2022-04-10 15:52:09 +0300

    What i do?

--- src/App.js
+++ src/App.js
@@ -1,18 +1,115 @@
 import logo from './logo.svg';
 import './App.css';
-
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 function App() {
   return (
     <div className="App">
+       <SimpleBottomNavigation></SimpleBottomNavigation>
       <header className="App-header">
+       
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Mazur Vsevolod CR-193
         </p>
-
+        <div>
+          <h1>Content</h1>
+        <BasicGrid></BasicGrid>
+        </div>
+        
       </header>
     </div>
   );
 }
 
 export default App;
+
+// import RestoreIcon from '@mui/icons-material/Restore';
+// import FavoriteIcon from '@mui/icons-material/Favorite';
+// import LocationOnIcon from '@mui/icons-material/LocationOn';
+ function SimpleBottomNavigation() {
+  const [value, setValue] = React.useState(0);
+
+  return (
+    <Box sx={{ width: 500 }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction label="Home"  />
+        <BottomNavigationAction label="About"  />
+        <BottomNavigationAction label="FAQ"  />
+      </BottomNavigation>
+    </Box>
+  );
+}
+
+
+function MediaCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+function BasicGrid() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+
+        <Grid item xs={4}>
+          <Item><MediaCard></MediaCard></Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item><MediaCard></MediaCard></Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item><MediaCard></MediaCard></Item>
+        </Grid>
+
+      </Grid>
+    </Box>
+  );
+}
